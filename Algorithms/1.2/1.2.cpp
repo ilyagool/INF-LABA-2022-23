4548ddecbccba2b41fc417361a541ae23c8b78b1
ilyagool <74778426+ilyagool@users.noreply.github.com>
2022-11-02 00:38:52 +0500

    Add files via upload

---
+++ Algorithms/1.2/1.2.cpp
@@ -0,0 +1,18 @@
+include <iostream>
+using namespace std;
+int main()
+{
+    float a, b;
+
+    cin >> a >> b;
+
+    if (b == 0)
+    {
+        cout << "На ноль делить нельзя!" << endl;
+    }
+    else
+    {
+        cout << a / b << endl;
+    }
+
+}
