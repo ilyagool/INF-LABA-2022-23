5f5fdb80e14dbb931e7d91f3b68e1c1a2e77a35c
ilyagool <74778426+ilyagool@users.noreply.github.com>
2022-11-02 14:17:56 +0500

    Add files via upload

---
+++ Algorithms/2.4/2.4.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+start:
+    float n;
+    int Probels;
+    int Stars = 1;
+    char probel = ' ';
+    char star = '*';
+    std::cout << "Введите основание равнобедренного треугольника N (нечетное и больше 3-ёх)" << std::endl;
+    std::cin >> n;
+
+    if ((n <= 3) or ((int)n != n) or ((int)n % 2 != 1))
+    {
+        std::cout << "Неверно!" << std::endl;
+        goto start;
+    }
+    Probels = n / 2;
+    std::cout << std::endl;
+
+    for (int i = 1; i <= n; i += 2)
+    {
+        for (int j = 1; j <= Probels; j++)
+        {
+            std::cout << probel;
+        }
+
+        for (int j = 1; j <= Stars; j++)
+        {
+            std::cout << star;
+        }
+
+        if (Stars != n)
+        {
+            std::cout << std::endl;
+        }
+
+        Probels--;
+        Stars += 2;
+    }
+
+    std::cout << std::endl;
+}
---
+++ Algorithms/2.5/2.5.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+start:
+    float n;
+    char star = '*';
+    char probel = ' ';
+    int Probels;
+    int Stars = 1;
+
+    std::cout << "Введите длину катетов N:" << std::endl;
+    std::cin >> n;
+
+    if ((n <= 2) or ((int)n != n))
+    {
+        std::cout << "Неверно!" << std::endl;
+        goto start;
+    }
+
+    Probels = n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        Probels--;
+        Stars = i;
+        for (int j = 1; j <= Probels; j++)
+        {
+            std::cout << probel;
+        }
+
+        for (int j = 1; j <= Stars; j++)
+        {
+            std::cout << star;
+        }
+
+        if (Stars < n)
+        {
+            std::cout << std::endl;
+        }
+
+        if (Stars == n)
+        {
+            break;
+        }
+    }
+}
