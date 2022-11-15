c9fa77f845f3dc781e86f990cd18218d021847dc
ilyagool <74778426+ilyagool@users.noreply.github.com>
2022-11-15 23:14:46 +0500

    Add files via upload

---
+++ Algorithms/2.15/2.15.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    int n, s, a;
+    cin >> n >> s;
+    if (n < 0)
+    {
+        cout << "Число должно быть > 0";
+    }
+    else
+    {
+        while (n)
+        {
+            a = n % 10;
+            if (s == a)
+            {
+                cout << "Есть"; break;
+            }
+            else
+            {
+                n = n / 10;
+            }
+        }
+        if (s = !n)
+        {
+            cout << "Нет";
+        }
+    }
+
+    return 0;
+}
