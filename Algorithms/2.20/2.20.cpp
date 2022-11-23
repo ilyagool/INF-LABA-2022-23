cf9b619af3402fa73d6fa9381da55b2591f66536
ilyagool <74778426+ilyagool@users.noreply.github.com>
2022-11-23 10:38:38 +0500

    Add files via upload

---
+++ Algorithms/2.20/2.20.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    float n, a, s;
+    bool flag = false;
+    cout << "Введите n" << endl; cin >> n; cout << "Введите s" << endl; cin >> s;
+    for (int i = 1; i <= n; i++)
+    {
+        a = sin(n + i / n);
+        if (s == a)
+        {
+            flag = true;
+        }
+    }
+    if (flag)
+    {
+        cout << "Есть";
+    }
+    else
+    {
+        cout << "Нет";
+    }
+
+
+    return 0;
+}
