601a837147671cf25e5b8cd7ea7be8884cff2399
ilyagool <74778426+ilyagool@users.noreply.github.com>
2022-12-18 19:08:33 +0500

    Add files via upload

---
+++ Algorithms/2.3/2.3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    int n;
+    cout << "Введите число N(натуральное)" << endl;
+    cin >> n;
+    int sum = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        int tmp = 1;
+        for (int j = i; j <= 2 * i; j++)
+        {
+            tmp *= j;
+        }
+        sum += tmp;
+    }
+    cout << sum << endl;
+}
+
