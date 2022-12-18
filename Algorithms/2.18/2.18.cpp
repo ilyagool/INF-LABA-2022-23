3a2a64f103532afccc2f0c7ab6da12abb1a03426
ilyagool <74778426+ilyagool@users.noreply.github.com>
2022-12-18 23:03:32 +0500

    Add files via upload

---
+++ Algorithms/2.18/2.18.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    int r = 0; // reversed
+
+    cin >> n;
+
+    while (n > 0)
+    {
+        r = r * 10 + n % 10;
+        n /= 10;
+    }
+    cout << r << endl;
+}
