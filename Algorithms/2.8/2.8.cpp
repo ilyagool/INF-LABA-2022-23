e9ebfe53df247c22f220464cc904dcf5c2e2a2a3
ilyagool <74778426+ilyagool@users.noreply.github.com>
2022-12-18 19:12:46 +0500

    Add files via upload

---
+++ Algorithms/2.8/2.8.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int n, stars;
+
+	cout << "Введите число N(натуральное и большее 2)" << endl;
+	cin >> n;
+
+	if (n <= 2)
+	{
+		cout << "Неверно!" << endl;
+	}
+
+	stars = 1;
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= stars; j++)
+		{
+			cout << "* ";
+		}
+
+		stars++;
+
+		cout << endl;
+
+	}
+}
