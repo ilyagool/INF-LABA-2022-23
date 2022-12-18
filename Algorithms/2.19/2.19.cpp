3e67079aa82b806e3c8d222196c39c3ba9232b5f
ilyagool <74778426+ilyagool@users.noreply.github.com>
2022-12-18 23:04:04 +0500

    Add files via upload

---
+++ Algorithms/2.19/2.19.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, s = 0;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 3 != 0)
+        {
+            s += i;
+        }
+        else
+        {
+            s -= i;
+        }
+    } cout << s;
+
+
+
+    return 0;
+}
