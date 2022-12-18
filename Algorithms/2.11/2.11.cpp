44ab932157871210ae0c7e23f51e08079d422b55
ilyagool <74778426+ilyagool@users.noreply.github.com>
2022-12-18 22:57:56 +0500

    Add files via upload

---
+++ Algorithms/2.11/2.11.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+
+    int n, tpm;
+    bool plusFirst = false;
+    bool minusFirst = false;
+
+    cout << "Введите длину последовательности:" << endl;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> tpm;
+        if (!plusFirst && !minusFirst)
+        {
+            if (tpm > 0)
+            {
+                plusFirst = true;
+            }
+            else if (tpm < 0)
+            {
+                minusFirst = true;
+            }
+        }
+    }
+
+    if (plusFirst)
+    {
+        cout << "Сначала ввели положительное число";
+    }
+    else if (minusFirst)
+    {
+        cout << "Сначала ввели отрицательное число";
+    }
+    else
+    {
+        cout << "Все были нулевыми";
+    }
+    cout << endl;
+}
