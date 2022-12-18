eb50aa3ab759a77301f63cca7eb7ea65093ec9df
ilyagool <74778426+ilyagool@users.noreply.github.com>
2022-12-18 19:10:10 +0500

    Add files via upload

---
+++ Algorithms/2.5/2.5.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    int n;
+    int spaces;
+    cout << "Введите длину катетов N(натуральное и большее 2):" << endl;
+    cin >> n;
+    
+    if (n <= 2)
+    {
+        cout << "Неверно!" << endl;
+    }
+
+    spaces = n;
+
+    for (int i = 0; i < n; i++)
+    {
+        spaces--;
+
+        for (int j = 1; j < n - 1; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
