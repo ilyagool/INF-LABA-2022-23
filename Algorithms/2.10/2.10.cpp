4fc66f8b34a46abeaa59311c2e30837349381ed0
ilyagool <74778426+ilyagool@users.noreply.github.com>
2022-12-18 22:57:22 +0500

    Add files via upload

---
+++ Algorithms/2.10/2.10.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int n, max, tpm;
+	cout << "Введите длину последовательности:" << endl;
+	cin >> n;
+	cout << "Введите первое число:" << endl;
+	cin >> max;
+
+	for (int i = 2; i <= n; i++)
+	{
+		cout << "Введите следующее число:" << endl;
+		cin >> tpm;
+
+		if (tpm > max)
+		{
+			max = tpm;
+		}
+	}
+
+	cout << "Максимальный элемент: " << max << endl;
+}
