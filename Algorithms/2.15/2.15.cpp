23130c051f3de7e7d0f26d99b73997745f8039a8
ilyagool <74778426+ilyagool@users.noreply.github.com>
2022-12-18 23:00:37 +0500

    Add files via upload

---
+++ Algorithms/2.15/2.15.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    int N, S, k;
+    bool flag = false;
+
+    cout << "Введите числа N и S" << endl;
+    cin >> N >> S;
+
+    while (N > 0 && !flag)
+    {
+        k = N % 10;
+        if (k == S)
+        {
+            flag = true;
+            cout << "Есть" << endl;
+        }
+        else
+        {
+            N /= 10;
+        }
+    }
+
+    if (!flag)
+    {
+        cout << "Нет" << endl;
+    }
+}
