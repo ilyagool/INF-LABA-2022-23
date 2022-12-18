b81f718b8af6715a7e054de24775545b68ef4c38
ilyagool <74778426+ilyagool@users.noreply.github.com>
2022-12-18 22:59:54 +0500

    Add files via upload

---
+++ Algorithms/2.14/2.14.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    int n, sum = 0;
+    cin >> n;
+    if (n < 0)
+    {
+        cout << "Число должно быть > 0";
+    }
+    else
+    {
+        while (n)
+        {
+            sum += n % 10;
+            n = n / 10;
+        }
+    } cout << sum;
+
+    return 0;
+}
