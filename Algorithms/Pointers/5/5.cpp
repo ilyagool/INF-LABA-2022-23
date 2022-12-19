e12eea430f2118f95dfa58efaa76bcca92dfaa41
ilyagool <74778426+ilyagool@users.noreply.github.com>
2022-12-19 23:03:18 +0500

    Add files via upload

---
+++ Algorithms/Pointers/5/5.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int n;
+	int fact = 1;
+	int* f = &fact;
+
+	std::cout << "Введите число" << std::endl;
+	std::cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		*f *= i;
+	}
+
+	std::cout << "Факториал числа " << n << " равен " << fact << std::endl;
+}
