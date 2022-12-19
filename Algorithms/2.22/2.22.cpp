a9001b424a7ed6ee8a03a61379078079fe8ecff3
ilyagool <74778426+ilyagool@users.noreply.github.com>
2022-12-19 16:33:33 +0500

    Add files via upload

---
+++ Algorithms/2.22/2.22.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    float n, a, pos, max = -1;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        a = sin(n + i / n);
+        if (a > max)
+        {
+            max = a;
+            pos = i;
+        }
+    } cout << "Максимальное элемент" << max << endl << "Его значение" << pos;
+
+
+    return 0;
+}
