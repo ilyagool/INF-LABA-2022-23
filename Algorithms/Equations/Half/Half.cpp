8806dfe4e6a35271c59dbfe39ad8ded8553917d1
ilyagool <74778426+ilyagool@users.noreply.github.com>
2022-12-20 19:56:56 +0500

    Add files via upload

---
+++ Algorithms/Equations/Half/Half.cpp
@@ -0,0 +1,34 @@
+﻿// Half
+// f(x) = x - (1/(3 + sin(3.6 * x)))
+// [0;0,85]
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	float a, b, c;
+	float fA, fB, fC;
+	double eps = 0.000000001;
+	a = 0;
+	b = 0, 85;
+	while (b - a > eps)
+	{
+		c = (a + b) / 2;
+		fA = a - (1 / (3 + sin(3.6 * a)));
+		fB = b - (1 / (3 + sin(3.6 * b)));
+		fC = c - (1 / (3 + sin(3.6 * c)));
+		if (fA * fC < 0)
+		{
+			b = c;
+		}
+		else if (fC * fB < 0)
+		{
+			a = c;
+		}
+		else { std:: cout << "Что-то не то!" << std :: endl; }
+	}
+	std :: cout << "Корни: " << a << "; " << b << std :: endl;
+	return 0;
+}
+
