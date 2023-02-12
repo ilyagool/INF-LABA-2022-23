223fe4caa12acff5c00c3621245494f13b73d983
ilyagool <74778426+ilyagool@users.noreply.github.com>
2023-02-12 14:45:30 +0500

    Add files via upload

---
+++ 2nd Semester/tower_of_hanoi/tower_of_hanoi.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+void towerOfHanoi(int n, char from, char to, char tmp)
+{
+    setlocale(LC_ALL, "RUS");
+    if (n == 0)
+    {
+        return;
+    }
+    towerOfHanoi(n - 1, from, tmp, to);
+    cout << "Переместить кольцо " << n << " из " << from << " в " << to << endl;
+    towerOfHanoi(n - 1, tmp, to, from);
+}
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    int n;
+
+    cout << "Введите количество колец: ";
+    cin >> n;
+    cout << endl;
+
+    towerOfHanoi(n, '1', '3', '2');
+    return 0;
+}
