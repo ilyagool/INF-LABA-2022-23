7fd36d59cab3d576410053a06576499e94fc1f13
ilyagool <74778426+ilyagool@users.noreply.github.com>
2023-02-19 11:39:20 +0500

    Add files via upload

---
+++ 2nd Semester/Labs_4/Labs_4.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+void del(int a[], int t)
+{
+	int n = 5;
+	t = 25;
+	for (int i = 0; i < t-2; i++)
+	{
+		if (i < t - n)
+		{
+			a[i] = a[i + n];
+		}
+		else
+		{
+			a[i] = rand() % 100;
+		}
+		cout << a[i] << " ";
+	}
+
+}
+
+int main()
+{
+	const int k = 25;
+	int m[k];
+
+	srand(time(NULL));
+
+	for (int i = 0; i < k; i++)
+	{
+		m[i] = rand() % 100;
+		cout << m[i] << " ";
+	}
+
+	cout << endl;
+	del(m, k);
+	
+	return 0;
+}
