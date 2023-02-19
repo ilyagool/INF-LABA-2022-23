605eee3f0f940a6583abcbdf291c7fd54d459e0b
ilyagool <74778426+ilyagool@users.noreply.github.com>
2023-02-19 19:46:44 +0500

    Add files via upload

---
+++ 2nd Semester/Labs_5/Labs_5.cpp
@@ -0,0 +1,103 @@
+﻿#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+bool searchNeg(int arr[], int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		if (arr[i] < 0)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+void moveNeg(int arr[][100], int r, int c)
+{
+	int newArr[100][100];
+	int p = 0, n = r - 1;
+	for (int i = 0; i < r; i++)
+	{
+		if (searchNeg(arr[i], c))
+		{
+			for (int j = 0; j < c; j++)
+			{
+				newArr[p][j] = arr[i][j];
+				newArr[n][j] = 0;
+				
+			}
+			n--;
+		}
+		else
+		{
+			for (int j = 0; j < c; j++)
+			{
+				newArr[p][j] = arr[i][j];
+			}
+			p++;
+		}
+	}
+
+	for (int i = 0; i < r; i++)
+	{
+		for (int j = 0; j < c; j++)
+		{
+			arr[i][j] = newArr[i][j];
+		}
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	srand(time(NULL));
+
+	int col, row, n;
+	cout << "Введите количество строк:";
+	cin >> row;
+	cout << "Введите количество столбцов:";
+	cin >> col;
+
+	int arr[100][100];
+	cout << "Исходный массив:" << endl;
+
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			arr[i][j] = rand() % 100 - 5;
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	cout << "Введите номер строки, которую хотите проверить:";
+	cin >> n;
+	if (searchNeg(arr[n-1], col))
+	{
+		cout << "Строка " << n << " содержит отрицательные элементы." << endl;
+	}
+	else
+	{
+		cout << "Строка " << n << " не содержит отрицательных элементов." << endl;
+	}
+
+	moveNeg(arr, row, col);
+	cout << "Новый массив:" << endl;
+
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
+	
+
+
---
+++ 2nd Semester/Labs_7.2/Labs_7.2.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <cmath>
+#include <stdarg.h>
+using namespace std;
+
+int kvadr(int amount, ...)
+{
+	va_list args;
+	va_start(args, amount);
+	int count = 0;
+
+	for (int i = 0; i < amount ; i++)
+	{
+		int num = va_arg(args, int);
+		int r = sqrt(num);
+		if (r * r == num)
+		{
+			count++;
+		}
+	}
+	va_end(args);
+	return count;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	cout << "Количество полных квадратов: " << kvadr(3, 2, 9, 64) << endl;
+
+	cout << "Количество полных квадратов: " << kvadr(7, 3, 4, 9, 15, 25, 36, 60) << endl;
+
+	cout << "Количество полных квадратов: " << kvadr(11, 1, 2, 4, 16, 17, 36, 60, 64, 78, 81, 100) << endl;
+
+	return 0;
+}
