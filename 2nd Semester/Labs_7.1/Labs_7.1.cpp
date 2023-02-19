b4e55afe7e67c63aa84e09621f5e2e8a7a84b35b
ilyagool <74778426+ilyagool@users.noreply.github.com>
2023-02-19 20:51:05 +0500

    Add files via upload

---
+++ 2nd Semester/Labs_7.1/Labs_7.1.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+using namespace std;
+
+int  NOD(int a, int b)
+{
+	while (a != 0 && b != 0)
+	{
+		if (a > b)
+		{
+			a %= b;
+		}
+		else
+		{
+			b %= a;
+		}
+	}
+	return (a + b);
+}
+
+void sub1(int A, int B, int C, int D)
+{
+	int sumN, sumD;
+	cout << A << "/" << B << " - " << C << "/" << D << endl;
+	sumN = (A * D) - (B * C);
+	sumD = (B * D);
+	cout << sumN << "/" << sumD << endl;
+	int x = NOD(sumN, sumD);
+	sumN /= x;
+	sumD /= x;
+	cout << sumN << "/" << sumD << endl;
+}
+
+void sub2(float a, float b)
+{
+	cout << (a - b) << endl;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int A, B, C, D;
+	float a, b;
+	cout << "Введите числитель и знаменатель первой дроби:" << endl;
+	cin >> A >> B;
+	cout << "Введите числитель и знаменатель второй дроби:" << endl;
+	cin >> C >> D;
+	sub1(A, B, C, D);
+	cout << "Введите два вещественных числа:" << endl;
+	cin >> a >> b;
+	sub2(a, b);
+
+	return 0;
+	
+}
