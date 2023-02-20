ea397cfd95cf5daf767e382ccb4baa366d280170
ilyagool <74778426+ilyagool@users.noreply.github.com>
2023-02-20 17:30:53 +0500

    Add files via upload

---
+++ 2nd Semester/Labs_6/Labs_6.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool defPal(string s)
+{
+	int first = 0;
+	int last = s.length() - 1;
+	while (first < last)
+	{
+		if (s[first] != s[last]) { return false; }
+		first++;
+		last--;
+	}
+	return true;
+}
+
+void printPal(string s)
+{
+	string word = "";
+	for (int i = 0; i < s.length(); i++)
+	{
+		if (s[i] == ' ')
+		{
+			if (defPal(word))
+			{
+				cout << word << " ";
+			}
+			word = "";
+		}
+		else
+		{
+			word += s[i];
+		}
+	}
+	if (defPal(word))
+	{
+		cout << word << " ";
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, ("RUS"));
+
+	string s;
+	cout << "Введите строку: ";
+	getline(cin, s);
+	cout << "Слова-палидромы: ";
+	printPal(s);
+
+	return 0;
+}
