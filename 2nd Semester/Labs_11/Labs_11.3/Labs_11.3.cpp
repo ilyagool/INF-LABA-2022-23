318b8777bb8745d8a39e302b70f7520a9af5ccb4
ilyagool <74778426+ilyagool@users.noreply.github.com>
2023-03-27 14:17:35 +0500

    Add files via upload

---
+++ 2nd Semester/Labs_11/Labs_11.3/Labs_11.3.cpp
@@ -0,0 +1,251 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Node
+{
+	int head;
+	Node* next;
+};
+
+struct Stack
+{
+	Node* node;
+	int size;
+	void init()
+	{
+		node = nullptr;
+		size = 0;
+	}
+};
+
+void push(Stack*& stack, int head)
+{
+	Node* tmp = new Node;
+	tmp->head = head;
+	tmp->next = stack->node;
+	stack->node = tmp;
+	stack->size++;
+}
+
+void pop(Stack*& stack)
+{
+	if (stack->node == nullptr)
+	{
+		return;
+	}
+	stack->node = stack->node->next;
+	stack->size--;
+}
+void add_element(Stack*& stack, int position, int head)
+{
+	Stack* temp = new Stack;
+	temp->init();
+	int init_size = stack->size;
+	if (position == 1)
+	{
+		push(stack, head);
+	}
+	else
+	{
+		for (int i = 0; i <= init_size - position; i++)
+		{
+			push(temp, stack->node->head);
+			pop(stack);
+		}
+		push(stack, head);
+		init_size = temp->size;
+		for (int i = 0; i < init_size; i++)
+		{
+			push(stack, temp->node->head);
+			pop(temp);
+		}
+	}
+	delete temp;
+}
+void add_Kelements(Stack*& stack, int position, int k)
+{
+	Stack* temp = new Stack;
+	temp->init();
+	int head;
+	int init_size = stack->size;
+	cout << "enter the element want to add: ";
+	if (position == 1)
+	{
+		for (int i = 0; i < k; i++)
+		{
+			cin >> head;
+			push(stack, head);
+		}
+	}
+	else
+	{
+		for (int i = 0; i <= init_size - position; i++)
+		{
+			push(temp, stack->node->head);
+			pop(stack);
+		}
+		for (int i = 0; i < k; i++)
+		{
+			cin >> head;
+			push(stack, head);
+		}
+		init_size = temp->size;
+		for (int i = 0; i < init_size; i++)
+		{
+			push(stack, temp->node->head);
+			pop(temp);
+		}
+	}
+	delete temp;
+}
+void delete_element(Stack*& stack, int position)
+{
+	Stack* temp = new Stack;
+	temp->init();
+	int init_size = stack->size;
+	if (position == 1)
+	{
+		pop(stack);
+	}
+	else
+	{
+		for (int i = 0; i < init_size - position; i++)
+		{
+			push(temp, stack->node->head);
+			pop(stack);
+		}
+		pop(stack);
+		init_size = temp->size;
+		for (int i = 0; i < init_size; i++)
+		{
+			push(stack, temp->node->head);
+			pop(temp);
+		}
+	}
+	delete temp;
+}
+
+void delete_Kelements(Stack*& stack, int position, int k)
+{
+	Stack* temp = new Stack;
+	temp->init();
+	int init_size = stack->size;
+	if (position == 1)
+	{
+		for (int i = 0; i < k; i++)
+		{
+			pop(stack);
+		}
+	}
+	else
+	{
+		for (int i = 0; i < init_size - position; i++)
+		{
+			push(temp, stack->node->head);
+			pop(stack);
+		}
+		for (int i = 0; i < k; i++)
+		{
+			pop(stack);
+		}
+		init_size = temp->size;
+		for (int i = 0; i < init_size; i++)
+		{
+			push(stack, temp->node->head);
+			pop(temp);
+		}
+	}
+	delete temp;
+}
+
+void search_element(Stack*& stack, int element)
+{
+	Stack* current = new Stack;
+	current->init();
+	while (stack->node != nullptr)
+	{
+		if (stack->node->head == element)
+		{
+			cout << "The element is found" << endl;
+			return;
+		}
+		else
+		{
+			push(current, stack->node->head);
+			pop(stack);
+		}
+	}
+	cout << "The element is not found" << endl;
+}
+
+
+Stack* createStack(int n)
+{
+	Stack* stack = new Stack;
+	stack->init();
+	int head;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> head;
+		push(stack, head);
+	}
+	return stack;
+}
+
+void printStack(Stack*& stack)
+{
+	Node* current = stack->node;
+	for (int i = 0; i < stack->size; i++)
+	{
+		cout << current->head << ' ';
+		current = current->next;
+	}
+	cout << endl;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int n, data, pos, k, elem;
+	cout << "enter the stack's size: ";
+	cin >> n;
+	cout << "enter the stack's elements: ";
+	Stack* stack = createStack(n);
+	cout << "output stack: ";
+	printStack(stack);
+	cout << "enter the element that want to be add: ";
+	cin >> data;
+	push(stack, data);
+	cout << "output stack: ";
+	printStack(stack);
+	pop(stack);
+	cout << "output stack: ";
+	printStack(stack);
+	cout << "enter the element and position where want to add that element: ";
+	cin >> data;
+	cin >> pos;
+	add_element(stack, pos, data);
+	cout << "output stack: ";
+	printStack(stack);
+	cout << "enter quantity and position where want to add: ";
+	cin >> k;
+	cin >> pos;
+	add_Kelements(stack, pos, k);
+	cout << "output stack: ";
+	printStack(stack);
+	cout << "enter the position from where want to delete element: ";
+	cin >> pos;
+	delete_element(stack, pos);
+	cout << "output stack: ";
+	printStack(stack);
+	cout << "enter quantity of elements and position from where want to delete: ";
+	cin >> k;
+	cin >> pos;
+	delete_Kelements(stack, pos, k);
+	cout << "output stack: ";
+	printStack(stack);
+	cout << "enter the element want to find: ";
+	cin >> elem;
+	search_element(stack, elem);
+	return 0;
+}
---
+++ 2nd Semester/Labs_11/Labs_11.4/Labs_11.4.cpp
@@ -0,0 +1,212 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Node
+{
+	int head;
+	Node* next;
+};
+
+struct Queue
+{
+	Node* front;
+	Node* back;
+	int size;
+	void init()
+	{
+		front = nullptr;
+		back = nullptr;
+		size = 0;
+	}
+};
+
+void push(Queue*& queue, int head)
+{
+	Node* temp = new Node;
+	temp->head = head;
+	temp->next = nullptr;
+	if (queue->size == 0)
+	{
+		queue->front = temp;
+		queue->back = temp;
+	}
+	else
+	{
+		queue->back->next = temp;
+		queue->back = temp;
+	}
+	queue->size++;
+}
+
+void pop(Queue*& queue)
+{
+	if (queue->size == 0)
+	{
+		return;
+	}
+	queue->front = queue->front->next;
+	queue->size--;
+}
+void insert_element(Queue*& queue, int position, int head)
+{
+	Queue* temp = new Queue;
+	temp->init();
+	for (int i = 1; i < position; i++)
+	{
+		push(temp, queue->front->head);
+		pop(queue);
+	}
+	push(temp, head);
+	int init_size = queue->size;
+	for (int i = 0; i < init_size; i++)
+	{
+		push(temp, queue->front->head);
+		pop(queue);
+	}
+	queue = temp;
+}
+void insert_Kelements(Queue*& queue, int position, int k)
+{
+	Queue* temp = new Queue;
+	temp->init();
+	int head;
+	cout << "enter elements want add: ";
+	for (int i = 1; i < position; i++)
+	{
+		push(temp, queue->front->head);
+		pop(queue);
+	}
+	for (int i = 0; i < k; i++)
+	{
+		cin >> head;
+		push(temp, head);
+	}
+	int init_size = queue->size;
+	for (int i = 0; i < init_size; i++)
+	{
+		push(temp, queue->front->head);
+		pop(queue);
+	}
+	queue = temp;
+}
+void delete_element(Queue*& queue, int position)
+{
+	Queue* temp = new Queue;
+	temp->init();
+	for (int i = 1; i < position; i++)
+	{
+		push(temp, queue->front->head);
+		pop(queue);
+	}
+	pop(queue);
+	int init_size = queue->size;
+	for (int i = 0; i < init_size; i++)
+	{
+		push(temp, queue->front->head);
+		pop(queue);
+	}
+	queue = temp;
+}
+
+void delete_Kelements(Queue*& queue, int position, int k)
+{
+	Queue* temp = new Queue;
+	temp->init();
+	for (int i = 1; i < position; i++)
+	{
+		push(temp, queue->front->head);
+		pop(queue);
+	}
+	for (int i = 0; i < k; i++)
+	{
+		pop(queue);
+	}
+	int init_size = queue->size;
+	for (int i = 0; i < init_size; i++)
+	{
+		push(temp, queue->front->head);
+		pop(queue);
+	}
+	queue = temp;
+}
+
+void search_element(Queue*& queue, int element)
+{
+	Queue* current = new Queue;
+	current->init();
+	while (queue->front != nullptr && queue->back != nullptr)
+	{
+		if (queue->front->head == element)
+		{
+			cout << "element is found" << endl;
+			return;
+		}
+		else
+		{
+			push(current, queue->front->head);
+			pop(queue);
+		}
+	}
+	cout << "element is not found" << endl;
+}
+
+Queue* createQueue(int n)
+{
+	Queue* queue = new Queue;
+	queue->init();
+	int head;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> head;
+		push(queue, head);
+	}
+	return queue;
+}
+
+void printQueue(Queue*& queue)
+{
+	Node* current = new Node;
+	current = queue->front;
+	for (int i = 0; i < queue->size; i++)
+	{
+		cout << current->head << ' ';
+		current = current->next;
+	}
+	cout << endl;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int n, data, pos, k, elem;
+	cout << "enter queue's size: ";
+	cin >> n;
+	cout << "enter queue's elements" << endl;
+	Queue* queue = createQueue(n);
+	cout << "output queue" << endl;
+	printQueue(queue);
+	cout << "enter element want to add: ";
+	cin >> data;
+	push(queue, data);
+	cout << "output queue: " << endl;
+	printQueue(queue);
+	pop(queue);
+	cout << "output queue: " << endl;
+	printQueue(queue);
+	cout << "enter quantity of elements and position where want to add: ";
+	cin >> k;
+	cin >> pos;
+	insert_Kelements(queue, pos, k);
+	cout << "output queue: " << endl;
+	printQueue(queue);
+	cout << "enter quantity of elements and position from where want to delete: ";
+	cin >> k;
+	cin >> pos;
+	delete_Kelements(queue, pos, k);
+	cout << "output queue: " << endl;
+	printQueue(queue);
+	cout << "enter element want to find: ";
+	cin >> elem;
+	search_element(queue, elem);
+	return 0;
+}
