8fda2370606f138112c52ad568dd423900811daa
ilyagool <74778426+ilyagool@users.noreply.github.com>
2023-03-27 13:34:40 +0500

    Add files via upload

---
+++ 2nd Semester/Search/interpolationSearch/interpolationSearch.cpp
@@ -0,0 +1,98 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+
+void* bubbleSort(int* arr, int size)
+{
+	int tmp;
+
+	for (int i = 0; i < size - 1; i++)
+	{
+		for (int j = 0; j < size - i - 1; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				tmp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;
+			}
+		}
+	}
+
+	return arr;
+}
+
+int interpolationSearch(int* arr, int key, int size)
+{
+	int mid = 0;
+	int left = 0;
+	int right = size;
+	bool found = false;
+
+	while ((left <= right) && !found)
+	{
+		mid = left + ((key - arr[left]) * (right - left)) / (arr[right] - arr[left]);
+		if (arr[mid] < key)
+		{
+			left = mid + 1;
+		}
+		else if (arr[mid] > key)
+		{
+			right = mid - 1;
+		}
+		else
+		{
+			found = true;
+		}
+	}
+	if (arr[left] == key)
+	{
+		return left;
+	}
+	else if (arr[right] == key)
+	{
+		return right;
+	}
+	else
+	{
+		return -1;
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	srand(time(NULL));
+	int size, key;
+	cout << "Введите размер массива: ";
+	cin >> size;
+	int* arr = new int[size];
+	cout << "Массив: ";
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = rand() % 101;
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+	cout << "Отсортированный массив: ";
+	bubbleSort(arr, size);
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+	cout << "Введите элемент, который хотите найти: ";
+	cin >> key;
+	cout << endl;
+	if (interpolationSearch(arr, key, size) == -1)
+	{
+		cout << "Элемент не найден" << endl;
+	}
+	else
+	{
+		cout << "Элемент найден под номером: " << interpolationSearch(arr, key, size) + 1 << endl;
+	}
+
+	delete[] arr;
+	return 0;
+}
---
+++ 2nd Semester/Search/lineSearch/lineSearch.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int lineSearch(int arr[], int key, const int n)
+{
+	int cnt = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] == key) { cnt += 1; }
+	}
+	return cnt;
+}
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int key, n, arr[1000];
+	srand(time(NULL));
+	cout << "Размер массива > "; 
+	cin >> n;
+	cout << "Искомый элемент > "; 
+	cin >> key;
+	cout << "Исходный массив: ";
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 10;
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+	cout << "Ответ: " << lineSearch(arr, key, n);
+
+	return 0;
+}
