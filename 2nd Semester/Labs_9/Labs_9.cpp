918a9ae2821948a214e3ba4a772b2f36e54111eb
ilyagool <74778426+ilyagool@users.noreply.github.com>
2023-03-27 12:27:03 +0500

    Add files via upload

---
+++ 2nd Semester/Labs_9/Labs_9.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+    ifstream fin("F1.txt");
+    ofstream fout("F2.txt");
+    string line;
+    string longestWord;
+
+    while (getline(fin, line)) {
+        bool containsOneWord = true;
+
+        for (char c : line) {
+            if (isspace(c)) {
+                containsOneWord = false;
+                break;
+            }
+        }
+
+        if (containsOneWord) {
+            fout << line << endl;
+            if (line.length() > longestWord.length()) {
+                longestWord = line;
+            }
+        }
+    }
+
+    fin.close();
+    fout.close();
+
+    cout << "Самое длинное слово в файле F2: " << longestWord << endl;
+
+    return 0;
+}
